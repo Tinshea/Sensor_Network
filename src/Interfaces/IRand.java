f17e4b2df0bc2e3c071a0dfda1849b3e92febf18
Malek <malekbouzarkouna58@gmail.com>
2024-01-30 00:52:12 +0100

    rand crand srand et comparateur fait

---
+++ src/Interfaces/IRand.java
@@ -0,0 +1,7 @@
+package Interfaces;
+
+import fr.sorbonne_u.cps.sensor_network.requests.interfaces.ProcessingNodeI;
+
+public interface IRand {
+      public double eval(ProcessingNodeI curentNode);
+}
--- src/Interfaces/ISRand.java
+++
@@ -1,5 +0,0 @@
-package Interfaces;
-
-public interface ISRand extends Irand {
-	String getsensorId();
-}
